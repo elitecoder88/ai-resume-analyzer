f9a4a93942526a6706f4abf9819cd1e0ed0cd3f2
elitecoder88 <cbiekeu@nmsu.edu>
2025-09-08 07:35:11 -0400

    Resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -19,12 +19,12 @@ const Resume = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(!isLoading && !auth.isAuthenticated) navigate(`/auth?next=resume/${id}`);
+    if(!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume/${id}`);
   }, [isLoading]);
 
   useEffect(() => {
     const loadResume = async () => {
-      const resume = await kv.get(`/resume/${id}`);
+      const resume = await kv.get(`resume:${id}`);
 
       if(!resume) return;
 
@@ -33,7 +33,7 @@ const Resume = () => {
       const resumeBlob = await fs.read(data.resumePath);
       if(!resumeBlob) return;
 
-      const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
+      const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' });
       const resumeUrl = URL.createObjectURL(pdfBlob);
       setResumeUrl(resumeUrl);
 
@@ -51,14 +51,14 @@ const Resume = () => {
 
   return (
     <main className="!pt-0">
-      <nav className="resume-val">
+      <nav className="resume-nav">
         <Link to="/" className="back-button">
           <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
           <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
         </Link>
       </nav>
-      <div className="felx felx-row w-full max-lg:flex-col-reverse">
-        <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center]">
+      <div className="flex flex-row w-full max-lg:flex-col-reverse">
+        <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
           {imageUrl && resumeUrl && (
             <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
               <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
@@ -72,14 +72,12 @@ const Resume = () => {
           )}
         </section>
         <section className="feedback-section">
-          <h2 className="text-4xl text-black font-bold">Resume Review</h2>
+          <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
           {feedback ? (
-            <div className="felx flex-col gap-8 animate-in fade-in duration-1000">
+            <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
               <Summary feedback={feedback} />
               <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
               <Details feedback={feedback} />
-
-
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" className="w-full" />
